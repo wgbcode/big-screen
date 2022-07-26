79f486288b84ff1a8701346bec634250328d636b
wuguangbin <2567199685@qq.com>
2022-07-26 20:37:52 +0800

    顶部图片

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,6 @@
-import "./main.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Home } from "./pages/home";
+import "./shared/style.scss";
+
+ReactDOM.render(<Home />, document.getElementById("root"));
---
+++ src/pages/home.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import header from "../images/header.png";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <header style={{ backgroundImage: `url(${header})` }}></header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
+      <footer></footer>
+    </div>
+  );
+};
+
+export { Home };
