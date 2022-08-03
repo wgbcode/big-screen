ce6af29a3984d1a491865df4d89f612ef6e4f2f1
wuguangbin <2567199685@qq.com>
2022-08-03 11:47:19 +0800

    chart5

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,6 +6,7 @@ import { Chart1 } from "../components/chart-1";
 import { Chart2 } from "../components/chart-2";
 import { Chart3 } from "../components/chart-3";
 import { Chart4 } from "../components/chart-4";
+import { Chart5 } from "../components/chart-5";
 
 const Home = () => {
   return (
@@ -20,7 +21,9 @@ const Home = () => {
           <Chart3 />
           <Chart4 />
         </section>
-        <section className="section3"></section>
+        <section className="section3 bordered">
+          <Chart5 />
+        </section>
         <section className="section4"></section>
         <section className="section5"></section>
       </main>
