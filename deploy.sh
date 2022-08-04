1d2e9a60e14dc298d052742d80bdc506b98f942e
wuguangbin <2567199685@qq.com>
2022-08-04 19:24:48 +0800

    配置根域名

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-yarn build &&
-cd dist &&
-git init &&
-git add . &&
-git commit -m 'deploy' &&
-git remote add origin git@github.com:wgbcode/big-screen.git &&
-git push -uf origin master:gh-pages &&
+set -e
+yarn build 
+cd dist 
+git init 
+git add -A
+git commit -m 'deploy' 
+git remote add origin git@github.com:wgbcode/big-screen.git 
+git push -uf origin master:gh-pages 
 cd -
